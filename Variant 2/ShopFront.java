19de2d13d1fcb6cc2d01fc3b4c284af59c872923
Aidan Hickey <aidan.hickey.749@my.csun.edu>
2023-11-09 10:51:43 -0800

    A New Beginning

---
+++ Variant 2/ShopFront.java
@@ -0,0 +1,13 @@
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+public class ShopFront extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
