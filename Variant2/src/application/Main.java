be1041d31c6fe68220d74b2d59255d39d1a15d18
Aidan Hickey <aidan.hickey.749@my.csun.edu>
2023-11-10 21:07:25 -0800

    more (error loading photo)

--- Variant2/src/application/Main.java
+++ Variant2/src/application/Main.java
@@ -9,11 +9,14 @@ import javafx.scene.layout.BorderPane;
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
+		primaryStage.setTitle("Sally's Sheds");
 		try {
 			BorderPane root = new BorderPane();
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
+			scene.getStylesheets().add
+			 (Main.class.getResource("application.css").toExternalForm());
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
